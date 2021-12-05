6601b5ab0d3fd10ccf021045087d9447104c9052
Shashank Athreya <athreya@MacBookAir-SA.local>
2021-12-05 17:12:22 +0100

    Fixed negative Q values and draw results

--- mcts.py
+++ mcts.py
@@ -12,7 +12,7 @@ class node(object):
         self.n_visit = None
 
     def __str__(self, level=0):
-        ret = "\t"*level+repr(self.value,self.Q_value)+"\n"
+        ret = "\t"*level+repr(self.value)+"\n"
         for child in self.children:
             ret += child.__str__(level+1)
         return ret
@@ -53,17 +53,29 @@ def isTerminal(order):
         return True
     return False
 
+def T3(order):
+    TicTacToe = ['','','','','','','','','']
+    for i in range(len(order)):
+        if i%2==0:
+            TicTacToe[order[i]] = 'X'
+        else:
+            TicTacToe[order[i]] = 'O'
+    return TicTacToe
+
 def getReward(order):
     if len(order)%2==1:
-        return(-1)
-    else:
         return(1)
+    else:
+        return(-1)
 
 states = [x for x in range(9)]
 def buidlTree(self):
     if isTerminal(self.curOrder):
         self.terminal = True
-        self.reward = getReward(self.curOrder)
+        if len(self.curOrder) == 9 and anyWinner(T3(self.curOrder)) is False:
+            self.reward = -1
+        else:
+            self.reward = getReward(self.curOrder)
         self.Q_value = self.reward
         self.n_Visit = 1
         return
@@ -95,41 +107,34 @@ def calcQ(self):
             qValue += ((child.reward + child.Q_value)/child.n_visit)
         self.Q_value = qValue
         return qValue
-    
-        
-    
 
 #%%
 #Starting constrction of tree and hardcoding the starting state
 #root node
 root = node(4)
 root.curOrder= [root.value]
-root.children = [node(1)]
-#child node
-step2 = root.children[0]
-step2.parent = root
-step2.children = [node(3)]
-step2.curOrder = step2.parent.curOrder + [step2.value]
-#g-child
-step3 = step2.children[0]
-step3.parent = step2
-step3.curOrder = step3.parent.curOrder + [step3.value]
-step3.children = [node(5)]
-#g2child
-step4 = step3.children[0]
-step4.parent = step3
-step4.curOrder = step4.parent.curOrder + [step4.value]
+# root.children = [node(1)]
+# #child node
+# step2 = root.children[0]
+# step2.parent = root
+# step2.children = [node(3)]
+# step2.curOrder = step2.parent.curOrder + [step2.value]
+# #g-child
+# step3 = step2.children[0]
+# step3.parent = step2
+# step3.curOrder = step3.parent.curOrder + [step3.value]
+# step3.children = [node(5)]
+# #g2child
+# step4 = step3.children[0]
+# step4.parent = step3
+# step4.curOrder = step4.parent.curOrder + [step4.value]
 # %%
-# Completing the tree
-buidlTree(step4)
-
-#%%
+# Completing the tree and calculating Q values
+buidlTree(root)
 print(calcQ(root))
-
-
 # %%
-print(step4.Q_value)
+print(root)
 # %%
-for child in step4.children[0].children[0].children:
-    print(child.value,child.Q_value)
+for child in root.children[1].children[6].children[0].children[0].children[2].children:
+    print(child.parent.value,child.value, child.Q_value,)
 # %%
